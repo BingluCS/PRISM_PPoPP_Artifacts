bc4c0ded2f385c6afd03fd69f2101671235418d1
BingluCS <112145032+BingluCS@users.noreply.github.com>
2025-11-22 23:00:56 +0800

    Update installation script

--- install-compressor.sh
+++ install-compressor.sh
@@ -1,19 +1,19 @@
-git submodule init
-git submodule update
-
 CUDA_ARCH="75;80;86;89;90"
 
 if [ ! -z "$1" ]; then
     CUDA_ARCH="$1"
 fi
 
-echo "\nsetting up cuSZp...\n"
+echo "\installing cuSZp2...\n"
+cd cuSZp
+git checkout 671d5f438f452f30192d333f206f3caa742f6350
+cd ..
 cmake -S cuSZp -B cuSZp/build \
     -D CMAKE_CUDA_ARCHITECTURES="$CUDA_ARCH" \
     -D CMAKE_BUILD_TYPE=Release
 cmake --build cuSZp/build -- -j
 
-echo "\nsetting up cuZFP...\n"
+echo "\ninstalling cuZFP...\n"
 cmake -S cuZFP -B cuZFP/build \
     -D ZFP_WITH_CUDA=on \
     -D CUDA_SDK_ROOT_DIR=$(dirname $(which nvcc))/.. \
@@ -21,6 +21,7 @@ cmake -S cuZFP -B cuZFP/build \
     -D CMAKE_BUILD_TYPE=Release 
 cmake --build cuZFP/build -- -j
 
+echo "\ninstalling cuSZ...\n"
 cmake -S cuSZ -B cuSZ/build \
     -D PSZ_BACKEND=cuda \
     -D PSZ_BUILD_EXAMPLES=off \
